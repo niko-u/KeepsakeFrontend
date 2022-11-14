9de09afaa699b55a478d758f04518b372ba967be
niko-u <niko@striker2k.com>
2022-11-14 13:57:13 -0600

    Added modal for posting audio on feed page

--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -1,4 +1,5 @@
 <template>
+    <button id="post" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Post a Memory</button>
      <div v-for="post in feedData" :key="post" class="item">
          <div class="postprofile">
             <img :src="post.postedBy.profilePicUrl" class="feedprofilepic">
@@ -10,6 +11,27 @@
              <button @click="WaveSurfer.playPause()">play</button>
          </div>
      </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Post a Memory!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <button type="button" class="btn btn-primary">Record</button>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Post</button>
+        </div>
+        </div>
+    </div>
+    </div>
+
 </template>
 
 <script>
@@ -111,5 +133,10 @@ export default {
 
     .card {
         
+    }
+
+    #post {
+        float: right;
+        margin-top: 200px;
     }
 </style>
