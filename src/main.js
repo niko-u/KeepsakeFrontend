95bcfa76965878edef68b59e03ff028f361b4aba
niko-u <niko@striker2k.com>
2022-11-13 23:01:18 -0600

    added dynamic vue elements to feed page

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import router from './router'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faPhone } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
 
 //library.add(faPhone)
+library.add(fas)
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -1,25 +1,43 @@
 <template>
-    <div class="item">
-        <div class="postprofile">
-            <img src="../assets/KeepsakeLogo.png" class="feedprofilepic">
-            <p>Name here</p>
-            <router-link to="/">Family name</router-link>
-        </div>
+     <div v-for="post in feedData" :key="post" class="item">
+         <div class="postprofile">
+            <img :src="post.postedBy.profilePicUrl" class="feedprofilepic">
+             <p> {{post.postedBy.firstName}} {{post.postedBy.lastName}}</p>
+             <router-link to="/">Family name</router-link>
+         </div>
         <div class="postcontent">
-            <VueWaveSurfer :src="file" :options="options"></VueWaveSurfer>
-            <button data-action="play">play</button>
-        </div>
-    </div>
+             <VueWaveSurfer :src="file" :options="options"></VueWaveSurfer>
+             <button @click="WaveSurfer.playPause()">play</button>
+         </div>
+     </div>
 </template>
 
 <script>
+
+
+    // <div class="item">
+    //     <div class="postprofile">
+    //         <img src="../assets/KeepsakeLogo.png" class="feedprofilepic">
+    //         <p>Name here</p>
+    //         <router-link to="/">Family name</router-link>
+    //     </div>
+    //     <div class="postcontent">
+    //         <VueWaveSurfer :src="file" :options="options"></VueWaveSurfer>
+    //         <button @click="WaveSurfer.playPause()">play</button>
+    //     </div>
+    // </div>
+
+
 import VueWaveSurfer from '../components/VueWaveSurfer/VueWaveSurfer.vue'
+
 export default {
   components: {
     VueWaveSurfer
   },
   data() {
     return {
+        importData: [],
+        feedData: [],
         options: {
             waveColor: '#a49eb1',
             progressColor: '#fbd2d0',
@@ -32,18 +50,41 @@ export default {
         },
         file: "src/assets/sample.mp3"
     }
+  },
+  async mounted() {
+    await this.getPostData();
+  },
+  methods: {
+    async getPostData() {
+
+        fetch('http://keepsake-env.eba-jndimye2.us-east-1.elasticbeanstalk.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            this.importData = data;
+            this.populateFeed(data)
+        })
+    },
+    async populateFeed(data) {
+        for (let i = 0; i < data.length; i++) {
+            this.feedData.push(data[i])
+            console.log(data[i])
+        }
+    }
   }
 }
 </script>
 
 <style>
     .item {
+        margin-top: 1.5%;
+        margin-bottom: 1%;
         background-color: #fff7e0;
         width: 80%;
         height: 250px;
         border-radius: 25px;
         border-color: black;
         border-width: 5px;
+        
     }
 
     .feedprofilepic {
@@ -66,5 +107,9 @@ export default {
         float: left;
         padding-left: 20%;
         padding-top: 2%;
+    }
+
+    .card {
+        
     }
 </style>
